a6a99d1fd4d2616936b64a7ebe46a4013ff4a604
Etimbuk Afia <ghostbandit02@gmail.com>
2024-10-14 20:55:17 +0100

    mongo connected

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 // IMPORTS
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
-import multer, { Multer } from 'multer';
+import multer from 'multer';
 import mongoose from 'mongoose';
 import Grid from 'gridfs-stream';
 import config from 'config';
 import { Readable } from 'stream';
-
+import connectToDB from './connect.js';
 
 // VARIABLES
 //const upload: Multer = multer({ storage: storage });  // multer for file storage
 const app: Express = express();
+connectToDB()
 const port = process.env.port || 4000;
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // APP METHODS
 app.use(express.json());
@@ -20,8 +23,20 @@ app.use(express.urlencoded({ extended: true })); // middleware function in Expre
 app.set("view engine", "ejs"); // Template engine to generate HTML dynamically using templates
 //ROUTES
 
+let connection = mongoose.connection;
 // code for uploading files
 // upload.single(‘file’): This middleware from multer handles single file uploads named “file” in the request
+
+connection.on("open", () => {
+    console.log("connection established successfully");
+    // Check if mongoose.connection.db is defined
+    if (mongoose.connection.db) {
+        let bucket = new mongoose.mongo.GridFSBucket(mongoose.connection.db);
+    } else {
+        console.error("Database connection is not established");
+    }
+});
+
 const uploadFiles = (req: Request, res: Response) => {
     try {
         res.status(200).json({ success: "file uploaded successfully" });
